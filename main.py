c2420be512a731ba71397589e205216b75379170
theMirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2024-06-12 06:13:18 +0500

    Commit

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ async def get_book_by_id(book_id: int):
     return [book for book in book_db if book["id"] == book_id]
 
 
-@app.get("/books")
+@app.get("/booklist")
 async def get_all_books():
     return book_db
+
+
+@app.post("/books")
+async def post_book_parameter(limit: int = 1, offset: int = 0):
+    return book_db[offset:][:limit]
